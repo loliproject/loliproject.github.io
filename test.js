0916404b0ca34f471f0a07cd904d707c842400fa
loliproject <76759033+loliproject@users.noreply.github.com>
2020-12-30 10:19:41 +0800

    test4

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
+function Bird() {
+	this.y = width / 2;
+	this.x = 25;
+
+	this.show = function() {
+		fill(255);
+		ellipse(this.x, this.y, 15, 15);
+	}
+}
+
+var bird;
+
 function setup() {
 	createCanvas(400, 600);
+	bird = new Bird();
 }
 
 function draw() {
 	background(0);
+	bird.show();
 }
